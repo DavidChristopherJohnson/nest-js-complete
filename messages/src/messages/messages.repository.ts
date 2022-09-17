b6d4d3df63953f4765b468aa78acc07123f487d8
David <dcjohnson85@gmail.com>
2022-09-17 13:33:35 +0100

    Added repo for messages (save to json file) - UNTESTED

---
+++ messages/src/messages/messages.repository.ts
@@ -0,0 +1,36 @@
+import { readFile, writeFile } from "fs/promises";
+
+export class MessagesRepository {
+    private readonly messageFile = 'messages.json';
+
+    async findOne(id: string) {
+        const contents = await this.readFileToJson(this.messageFile);
+
+        return contents[id];
+    }
+
+    async findAll() {
+        return await this.readFileToJson(this.messageFile);
+    }
+
+    async create(content: string) {
+        const contents = await this.readFileToJson(this.messageFile);
+
+        const id = Math.floor(Math.random() * 999);
+        contents[id] = {
+            id,
+            content
+        };
+
+        await this.writeJsonToFile(this.messageFile, contents);
+    }
+
+    private async readFileToJson(fileName: string, encoding: BufferEncoding = 'utf-8') {
+        const contents = await readFile(fileName, encoding)
+        return JSON.parse(contents);
+    }
+
+    private async writeJsonToFile(fileName: string, contents) {
+        await writeFile(fileName, JSON.stringify(contents));
+    }
+}

e4704e74ae3a67573f784aba9a998ac00003d394
David <dcjohnson85@gmail.com>
2023-02-20 20:13:51 +0000

    Adding initial signup implementation, without hashing

--- my-car-value/src/users/auth.service.ts
+++ my-car-value/src/users/auth.service.ts
@@ -1,7 +1,25 @@
-import { Injectable } from "@nestjs/common";
+import { BadRequestException, Injectable } from "@nestjs/common";
 import { UsersService } from "./users.service";
 
 @Injectable()
 export class AuthService {
     constructor(private usersService: UsersService){}
+
+    async signUp(email: string, password: string) {
+        const existingUser = await this.usersService.find(email);
+
+        if(existingUser) {
+            throw new BadRequestException('Email in use');
+        }
+
+        //TODO: replace this with a hashed password implementation
+        const hashedPassword = password;
+        const user = await this.usersService.create(email, password)
+
+        return user;
+    }
+
+    signIn(){
+
+    }
 }

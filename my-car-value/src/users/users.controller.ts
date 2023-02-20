f91dcdbaf81abf62cd92d994a390bdb871b7a46d
David <dcjohnson85@gmail.com>
2023-02-20 19:45:52 +0000

    Added customised interceptor

--- my-car-value/src/users/users.controller.ts
+++ my-car-value/src/users/users.controller.ts
@@ -7,6 +7,7 @@ import { CreateUserDto } from './dtos/create-user.dto';
 import { UpdateUserDto } from './dtos/update-user.dto';
 import { UsersService } from './users.service';
 import { SerializeInterceptor } from '../interceptors/serialize.interceptor';
+import { UserDto } from './dtos/user.dto';
 
 @Controller('auth')
 export class UsersController {
@@ -16,11 +17,11 @@ export class UsersController {
         this.usersService.create(body.email, body.password);
     }
 
-    @UseInterceptors(SerializeInterceptor)
+    @UseInterceptors(new SerializeInterceptor(UserDto))
     @Get('/:id')
     async findUser(@Param('id') id: string) {
         console.log("Handler is running")
-        
+
         const user = await this.usersService.findOne(parseInt(id));
 
         if (!user) {

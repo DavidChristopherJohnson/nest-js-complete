62f7a6b47b926fea9038661dcd2a3f9c8add568a
David <dcjohnson85@gmail.com>
2023-02-20 19:56:33 +0000

    Tweaks to interceptor implementation

--- my-car-value/src/users/users.controller.ts
+++ my-car-value/src/users/users.controller.ts
@@ -1,15 +1,16 @@
 import {
-    Body, Controller, Post, Get, Patch, Param, Query, Delete, NotFoundException,
-    UseInterceptors
+    Body, Controller, Post, Get, Patch, Param, 
+    Query, Delete, NotFoundException,
 } from '@nestjs/common';
 
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UpdateUserDto } from './dtos/update-user.dto';
 import { UsersService } from './users.service';
-import { SerializeInterceptor } from '../interceptors/serialize.interceptor';
+import { Serialize } from '../interceptors/serialize.interceptor';
 import { UserDto } from './dtos/user.dto';
 
 @Controller('auth')
+@Serialize(UserDto)
 export class UsersController {
     constructor(private usersService: UsersService) { }
     @Post('/signup')
@@ -17,11 +18,8 @@ export class UsersController {
         this.usersService.create(body.email, body.password);
     }
 
-    @UseInterceptors(new SerializeInterceptor(UserDto))
     @Get('/:id')
     async findUser(@Param('id') id: string) {
-        console.log("Handler is running")
-
         const user = await this.usersService.findOne(parseInt(id));
 
         if (!user) {

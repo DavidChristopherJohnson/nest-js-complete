ddd13008f274bc3d4a81e1429636117fff980da7
David <dcjohnson85@gmail.com>
2023-02-22 19:31:58 +0000

    Added password hashing to signup process

--- my-car-value/src/users/auth.service.ts
+++ my-car-value/src/users/auth.service.ts
@@ -1,5 +1,9 @@
 import { BadRequestException, Injectable } from "@nestjs/common";
 import { UsersService } from "./users.service";
+import { randomBytes, scrypt as _scrypt} from "crypto";
+import { promisify } from "util";
+
+const scrypt = promisify(_scrypt);
 
 @Injectable()
 export class AuthService {
@@ -8,13 +12,21 @@ export class AuthService {
     async signUp(email: string, password: string) {
         const existingUser = await this.usersService.find(email);
 
-        if(existingUser) {
+        if(existingUser.length) {
             throw new BadRequestException('Email in use');
         }
 
-        //TODO: replace this with a hashed password implementation
-        const hashedPassword = password;
-        const user = await this.usersService.create(email, password)
+        //Generate Salt
+        const salt = randomBytes(8).toString('hex');
+        
+        //Hash salt and password together (32 represents the length of the hash)
+        const hash =(await scrypt(password, salt, 32)) as Buffer;
+
+        //Join hash and salt together
+        const result  = salt + '.' + hash.toString('hex');    
+
+        //Create user, using the hashed and salted value as our password
+        const user = await this.usersService.create(email, result);
 
         return user;
     }

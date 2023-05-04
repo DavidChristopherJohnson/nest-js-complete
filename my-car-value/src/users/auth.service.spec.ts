417e2a13c4e60e1c2e9446037614f6a81e455abd
David <dcjohnson85@gmail.com>
2023-05-04 20:52:41 +0100

    Added some testing utilising more advanced mocking

--- my-car-value/src/users/auth.service.spec.ts
+++ my-car-value/src/users/auth.service.spec.ts
@@ -7,11 +7,21 @@ import { BadRequestException, NotFoundException } from '@nestjs/common';
 describe('AuthService', () => {
     let service: AuthService;
     let fakeUsersService: Partial<UsersService>;
+
     beforeEach(async () => {
+        const users: User[] = [];
+        
         //Create a fake copy of the users service
         fakeUsersService = {
-            find: () => Promise.resolve([]),
-            create: (email: string, password: string) => Promise.resolve({ id: 123, email, password } as User)
+            find: (email: string) => {
+                const filteredUsers = users.filter(user => user.email == email);
+                return Promise.resolve(filteredUsers);
+            },
+            create: (email: string, password: string) => {
+                const user: User = {id: Math.floor(Math.random() * 999999), email, password} as User;
+                users.push(user)
+                return Promise.resolve(user);
+            }
         };
 
         const module = await Test.createTestingModule({
@@ -28,11 +38,11 @@ describe('AuthService', () => {
     it('can create an instance of auth serice', () => {
         expect(service).toBeDefined();
     })
-    
-    it('creates a new user with a salted and hashed password', async() => {
+
+    it('creates a new user with a salted and hashed password', async () => {
         const user = await service.signUp('asdf@here.com', 'asdf');
 
-        expect(user.id).toBe(123);
+        expect(user.id).toEqual(expect.any(Number));
         expect(user.email).toBe('asdf@here.com');
         expect(user.password).not.toEqual('asdf');
         const [salt, hash] = user.password.split('.');
@@ -41,20 +51,23 @@ describe('AuthService', () => {
     });
 
     it('throws a bad request error if user signs up with email already in use', async () => {
-        fakeUsersService.find = () => Promise.resolve([{id: 123, email: 'a'}] as User[]);
-        await expect(service.signUp('asfd@here.com', 'asdf')).rejects.toThrow(BadRequestException,);
+        await service.signUp('asdf@here.com', 'asdf');
+        await expect(service.signUp('asdf@here.com', 'asdf')).rejects.toThrow(BadRequestException,);
     })
 
-    it('shrows if signin called with unused email', async() => {
+    it('throws if signin called with unused email', async () => {
         await expect(service.signIn('asdf@here.com', 'asdf')).rejects.toThrow(NotFoundException);
     })
 
-    it('signs in user correctly and returns user details', async () => {
-        fakeUsersService.find = () => Promise.resolve([{id: 123, email: 'asdf@here.com', password:'asdf.ghjk'}] as User[]);
+    it('throws if an invalid password is entered', async () => {
+        await service.signUp('asdf@here.com','asdf');
+        await expect(service.signIn('asdf@here.com', 'aasds')).rejects.toThrow(BadRequestException);
+    })
 
+    it('returns a user if correct password is provided', async () => {
+        await service.signUp('asdf@here.com', 'asdf');
         const user = await service.signIn('asdf@here.com', 'asdf');
         expect(user).toBeDefined;
-        expect(user.id).toBe(123);
         expect(user.email).toBe('asdf@here.com');
     })
 })

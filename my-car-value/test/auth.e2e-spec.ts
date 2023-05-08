4dd4d5eb6aefd382b06877a6ab124d836284bf6c
David <dcjohnson85@gmail.com>
2023-05-08 19:57:33 +0100

    Added auth e2e test, including config and cookie retrieval

--- my-car-value/test/auth.e2e-spec.ts
+++ my-car-value/test/auth.e2e-spec.ts
@@ -31,4 +31,25 @@ describe('Authentication System (e2e)', () => {
             expect(email).toBe(initialEmail)
         })
   });
+
+  it('signup as a new user then get the currently logged in user', async () => {
+    const initialEmail = "asdf@asdf.com";
+
+    const res = await request(app.getHttpServer())
+        .post('/auth/signup')
+        .send({
+            email: initialEmail,
+            password: 'asdf'
+        })
+        .expect(201)    
+        
+    const cookie = res.get('Set-Cookie');
+
+    const {body} = await request(app.getHttpServer())
+        .get('/auth/whoami')
+        .set('Cookie', cookie)
+        .expect(200)
+
+    expect(body.email).toEqual(initialEmail);
+  })
 });
